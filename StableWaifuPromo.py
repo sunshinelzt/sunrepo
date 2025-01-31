9867c10526315db8fda9bdc579e3b1404ca1c660
sunshinelzt <sunshine.prv@gmail.com>
2025-01-31 22:17:22 +0100

    Update StableWaifuPromo.py

--- StableWaifuPromo.py
+++ StableWaifuPromo.py
@@ -1,15 +1,16 @@
 # meta developer: @sunshinelzt
+
 from telethon import events
 from hikkatl.types import Message
 from .. import loader, utils
 
 class StableWaifuPromo(loader.Module):
-    """✨ Автоматическая активация промокодов из @StableWaifu"""
+    """Автоматическая активация промокодов из @StableWaifu"""
 
     strings = {"name": "StableWaifuPromo"}
 
     async def client_ready(self, client, db):
-        """⚙️ Инициализация"""
+        """Инициализация"""
         self.client = client
         self.db = db
         self.channel = "@StableWaifu"
@@ -17,23 +18,24 @@ class StableWaifuPromo(loader.Module):
         if self.db.get("StableWaifuPromo", "enabled", False):
             self.client.add_event_handler(self.check_new_messages, events.NewMessage(chats=self.channel))
 
-    async def wacmd(self, message: Message):
-        """🔹 Включает / Выключает автоактивацию"""
+    async def wcmd(self, message: Message):
+        """Включает / Выключает автоактивацию"""
         args = utils.get_args_raw(message)
+
         if args == "on":
             self.db.set("StableWaifuPromo", "enabled", True)
             self.client.add_event_handler(self.check_new_messages, events.NewMessage(chats=self.channel))
-            return await utils.answer(message, "✅ **Мониторинг @StableWaifu ВКЛЮЧЕН!**")
+            return await utils.answer(message, "**✅ Автоактивация ВКЛЮЧЕНА!**")
         elif args == "off":
             self.db.set("StableWaifuPromo", "enabled", False)
             self.client.remove_event_handler(self.check_new_messages)
-            return await utils.answer(message, "⛔ **Мониторинг ВЫКЛЮЧЕН.**")
+            return await utils.answer(message, "**⛔ Автоактивация ВЫКЛЮЧЕНА.**")
 
-        status = "🟢 **ВКЛЮЧЕН**" if self.db.get("StableWaifuPromo", "enabled", False) else "🔴 **ВЫКЛЮЧЕН**"
-        await utils.answer(message, f"📡 Мониторинг **@StableWaifu**: {status}")
+        status = "**🟢 ВКЛЮЧЕНА**" if self.db.get("StableWaifuPromo", "enabled", False) else "**🔴 ВЫКЛЮЧЕНА**"
+        await utils.answer(message, f"**📡 Статус автоактивации: {status}**")
 
     async def check_new_messages(self, event: Message):
-        """📥 Отслеживает новые промокоды и активирует их"""
+        """Отслеживает новые промокоды и активирует их"""
         promo_links = [e.url for e in event.message.entities or [] if hasattr(e, "url") and "start=promo_" in e.url]
 
         for link in promo_links:
