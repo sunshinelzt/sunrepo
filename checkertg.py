2393c6615dd3fbbab3f63e5b36d73423ead77ed1
sunshinelzt <sunshine.prv@gmail.com>
2025-02-03 06:20:15 +0100

    Update checkertg.py

--- checkertg.py
+++ checkertg.py
@@ -28,21 +28,19 @@ class CheckerTGMod(loader.Module):
         """Получаем user ID по username с использованием Telegram API"""
         try:
             entity = await client.get_entity(username)
-            return entity.id, entity.username  # Возвращаем ID и тэг
+            return entity.id, entity.username
         except Exception as e:
-            return None, None  # Если ошибка, возвращаем None для обоих значений
+            return None, None
 
     @loader.owner
     async def checkcmd(self, m):
         """Основная команда для проверки пользователя на слитый номер"""
         reply = await m.get_reply_message()
-        # Получаем идентификатор пользователя, если он передан в аргументах или в ответе на сообщение
         user_input = utils.get_args_raw(m) or (reply.sender_id if reply else None)
 
         if not user_input:
             return await m.edit(self.strings["no_user"])
 
-        # Если передан username, пытаемся получить его ID и тег
         if isinstance(user_input, str) and user_input.startswith("@"):
             await m.edit(self.strings["getting_id"])
             user_id, tag = await self.get_user_id(user_input, m.client)
@@ -50,28 +48,24 @@ class CheckerTGMod(loader.Module):
                 return await m.edit(self.strings["user_not_found"].format(user_input))
         else:
             user_id = str(user_input)
-            tag = None  # Если ID, то тэг не нужен
+            tag = None
 
-        # Отправляем запрос к API для проверки
         await m.edit(self.strings["checking"])
 
         url = f"https://api.d4n13l3k00.ru/tg/leaked/check?uid={user_id}"
 
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    # Проверяем статус ответа
+                async with session.get(url) as
                     if resp.status != 200:
                         raise ValueError(f"HTTP {resp.status}")
                     data = await resp.json()
 
-            # Извлекаем только номер телефона из данных
             phone_number = data.get("data", "").split(" | ")[0].replace("Phone: ", "")
             if "Not found!" in phone_number:
                 phone_number = "Не найдено"
 
 
-            # Формируем ответ с результатами проверки, включая только номер телефона
             result_message = self.strings["response"].format(
                 user_id=user_id,
                 phone_number=phone_number,
@@ -80,5 +74,4 @@ class CheckerTGMod(loader.Module):
             await m.edit(result_message)
 
         except Exception as e:
-            # Логируем ошибку и показываем её пользователю
             await m.edit(self.strings["error"].format(str(e)))
