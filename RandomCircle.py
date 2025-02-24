9a73176e6e1bc397b7720d79451ba1d55e626a95
sunshinelzt <sunshine.prv@gmail.com>
2025-02-24 18:37:12 +0100

    Update RandomCircle.py

--- RandomCircle.py
+++ RandomCircle.py
@@ -1,60 +1,54 @@
 # meta developer: @sunshinelzt
 
 import random
-from telethon.tl.types import InputPeerChannel, MessageMediaDocument
+import asyncio
+from telethon.tl.types import MessageMediaDocument, DocumentAttributeVideo
 from telethon.tl.functions.messages import GetHistoryRequest
 
-from .. import loader, utils
+from .. import loader
+
 
 @loader.tds
 class RandomCircleMod(loader.Module):
-    """Модуль для отправки случайного мемного кружочка"""
+    """Отправляет случайный кружочек."""
 
     strings = {"name": "RandomCircle"}
 
-    async def rccmd(self, message):
-        """Отправляет случайный кружочек."""
+    def __init__(self):
+        self.circles = []
 
-        channels_ids = [
-            -1001678673876,
-            -1001829766952,
-            -1001641159794,
-            -1001719785599,
-            -1001549768481,
-            -1001345824533,
+    async def fetch_circles(self, client, channel_id):
+        circles = []
+        try:
+            offset_id = 0
+            while True:
+                history = await client(GetHistoryRequest(channel_id, 100, offset_id=offset_id, hash=0))
+                if not history.messages:
+                    break
+                circles.extend(
+                    msg.media.document for msg in history.messages
+                    if isinstance(msg.media, MessageMediaDocument) and
+                    any(isinstance(attr, DocumentAttributeVideo) and attr.round_message
+                        for attr in msg.media.document.attributes)
+                )
+                offset_id = history.messages[-1].id
+        except:
+            pass
+        return circles
+
+    async def update_circles(self, client):
+        channels = [
+            -1001678673876, -1001829766952, -1001641159794,
+            -1001719785599, -1001549768481, -1001345824533,
             -1001869062957
         ]
+        results = await asyncio.gather(*[self.fetch_circles(client, cid) for cid in channels])
+        self.circles = [c for r in results if isinstance(r, list) for c in r]
 
-        try:
-            random_channel_id = random.choice(channels_ids)
-            entity = await message.client.get_entity(random_channel_id)
-            peer = InputPeerChannel(entity.id, entity.access_hash)
-
-            history = await message.client(
-                GetHistoryRequest(
-                    peer=peer,
-                    limit=100,
-                    offset_date=None,
-                    offset_id=0,
-                    max_id=0,
-                    min_id=0,
-                    add_offset=0,
-                    hash=0,
-                )
-            )
-
-            circles = [
-                msg for msg in history.messages 
-                if isinstance(msg.media, MessageMediaDocument) and 
-                msg.media.document.attributes and 
-                any(attr for attr in msg.media.document.attributes if attr.__class__.__name__ == "DocumentAttributeVideo" and getattr(attr, 'round_message', False))
-            ]
-
-            if circles:
-                random_circle = random.choice(circles)
-                await message.client.send_file(message.chat_id, file=random_circle.media.document)
-            
-            await message.delete()
-
-        except:
-            pass  # Игнорируем все ошибки
+    async def rccmd(self, message):
+        """Отправляет случайный кружочек."""
+        if not self.circles:
+            await self.update_circles(message.client)
+        if self.circles:
+            await message.client.send_file(message.chat_id, random.choice(self.circles))
+        await message.delete()
