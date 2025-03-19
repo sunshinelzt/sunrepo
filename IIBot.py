a55f202570596126ddd64f5d5de11130b08c7abe
sunshinelzt <sunshine.prv@gmail.com>
2025-03-19 22:37:05 +0100

    Update IIBot.py

--- IIBot.py
+++ IIBot.py
@@ -6,112 +6,118 @@ from .. import loader, utils
 
 @loader.tds
 class iibotMod(loader.Module):
-    """Модуль для включения режима дурачка в чате"""
-    
+    """Бот-дурачок, который отвечает на сообщения в чате"""
     strings = {
-        "name": "iibot",
+        "name": "IIBot",
         "pref": "<b>ii</b> ",
         "need_arg": "{}Нужен аргумент",
         "status": "{}{}",
         "on": "{}Включён",
         "off": "{}Выключен",
-        "no_track_users_desc": "Список пользователей, которых бот будет игнорировать",
+        "blacklist_add": "{}Добавлен в чёрный список: {}",
+        "blacklist_remove": "{}Удалён из чёрного списка: {}",
+        "blacklist_list": "{}Чёрный список пользователей: {}",
     }
     _db_name = "iibot"
 
     def __init__(self):
         self.config = loader.ModuleConfig(
-            loader.ConfigValue(
-                "no_track_users",
-                [],
-                doc=lambda: self.strings("no_track_users_desc"),
-                validator=loader.validators.Series(
-                    loader.validators.Union(loader.validators.String(), loader.validators.Integer())
-                ),
-            ),
-            loader.ConfigValue(
-                "chance",
-                0,
-                "Шанс ответа (0 - всегда отвечает, N - 1 к N)"
-            ),
+            "blacklist", [], "Список пользователей в чёрном списке"
         )
-
-    @property
-    def blacklist(self) -> set:
-        """Возвращает черный список пользователей в виде множества для быстрой проверки"""
-        return set(map(str, self.config["no_track_users"]))
-
-    @property
-    def chance(self) -> int:
-        """Возвращает шанс ответа"""
-        return self.config["chance"]
 
     async def client_ready(self, _, db):
         self.db = db
 
+    @staticmethod
+    def str2bool(v: str) -> bool:
+        return v.lower() in {"yes", "y", "true", "1", "on", "enable", "start", "да"}
+
     async def iicmd(self, m: types.Message):
         """Переключить режим дурачка в чате"""
-        args = utils.get_args_raw(m)
         if not m.chat:
             return
 
-        chat = m.chat.id
+        args = utils.get_args_raw(m)
+        chat_id = m.chat.id
         chats = set(self.db.get(self._db_name, "chats", []))
 
-        if args.lower() in ("on", "включить", "yes", "да", "1"):
-            chats.add(chat)
-            self.db.set(self._db_name, "chats", list(chats))
-            return await utils.answer(m, self.strings["on"].format(self.strings["pref"]))
+        if self.str2bool(args):
+            chats.add(chat_id)
+            await utils.answer(m, self.strings("on").format(self.strings("pref")))
+        else:
+            chats.discard(chat_id)
+            await utils.answer(m, self.strings("off").format(self.strings("pref")))
 
-        chats.discard(chat)
         self.db.set(self._db_name, "chats", list(chats))
-        return await utils.answer(m, self.strings["off"].format(self.strings["pref"]))
 
     async def randomicmd(self, m: types.Message):
-        """Установить шанс 1 к N. 0 - всегда отвечает"""
+        """Установить шанс 1 к N.\n0 - всегда отвечать."""
         args = utils.get_args_raw(m)
         if args.isdigit():
-            self.config["chance"] = int(args)
-            return await utils.answer(m, self.strings["status"].format(self.strings["pref"], args))
-        return await utils.answer(m, self.strings["need_arg"].format(self.strings["pref"]))
+            self.db.set(self._db_name, "chance", int(args))
+            return await utils.answer(m, self.strings("status").format(self.strings("pref"), args))
+
+        return await utils.answer(m, self.strings("need_arg").format(self.strings("pref")))
+
+    async def blacklistcmd(self, m: types.Message):
+        """Добавить/удалить пользователя из чёрного списка"""
+        reply = await m.get_reply_message()
+        args = utils.get_args_raw(m)
+
+        if reply:
+            user_id = reply.sender_id
+        elif args.isdigit():
+            user_id = int(args)
+        else:
+            return await utils.answer(m, self.strings("need_arg").format(self.strings("pref")))
+
+        blacklist = set(self.config["BLACKLIST"])
+
+        if user_id in blacklist:
+            blacklist.remove(user_id)
+            self.config["BLACKLIST"] = list(blacklist)
+            return await utils.answer(m, self.strings("blacklist_remove").format(self.strings("pref"), user_id))
+        else:
+            blacklist.add(user_id)
+            self.config["BLACKLIST"] = list(blacklist)
+            return await utils.answer(m, self.strings("blacklist_add").format(self.strings("pref"), user_id))
 
     async def watcher(self, m: types.Message):
         if not isinstance(m, types.Message) or not m.chat:
             return
 
-        user_id = str(m.sender_id)
         chat_id = m.chat.id
-
-        if user_id in self.blacklist:
-            return
+        user_id = m.sender_id
 
         if chat_id not in self.db.get(self._db_name, "chats", []):
             return
-
-        if self.chance and random.randint(0, self.chance) != 0:
+        if user_id == (await m.client.get_me()).id:
+            return
+        if user_id in self.config["blacklist"]:
             return
 
-        text = m.raw_text
-        if not text:
+        ch = self.db.get(self._db_name, "chance", 0)
+        if ch != 0 and random.randint(0, ch) != 0:
             return
 
-        words = [w for w in text.split() if len(w) >= 3]
+        words = [word for word in m.raw_text.split() if len(word) >= 3]
         if not words:
             return
 
-        keyword = random.choice(words)
-        messages = [msg async for msg in m.client.iter_messages(chat_id, search=keyword) if msg.replies and msg.replies.max_id]
-
-        if not messages:
+        search_word = random.choice(words)
+        msgs = [
+            x async for x in m.client.iter_messages(chat_id, search=search_word) if x.replies and x.replies.max_id
+        ]
+        if not msgs:
             return
 
-        replier = random.choice(messages)
+        replier = random.choice(msgs)
         sid, eid = replier.id, replier.replies.max_id
-
-        replies = [
-            msg async for msg in m.client.iter_messages(chat_id, ids=list(range(sid + 1, eid + 1)))
-            if msg and msg.reply_to and msg.reply_to.reply_to_msg_id == sid
+        reply_msgs = [
+            x async for x in m.client.iter_messages(chat_id, ids=list(range(sid + 1, eid + 1)))
+            if x and x.reply_to and x.reply_to.reply_to_msg_id == sid
         ]
+        if not reply_msgs:
+            return
 
-        if replies:
-            await m.reply(random.choice(replies))
+        await m.reply(random.choice(reply_msgs))
