9796aea60a8deaeb30c829d348eb3cc75c3ec332
sunshinelzt <sunshine.prv@gmail.com>
2025-03-21 21:26:16 +0100

    Update WarPigs.py

--- WarPigs.py
+++ WarPigs.py
@@ -5,13 +5,14 @@ import asyncio
 
 class WarpigsMod(loader.Module):
     """Автоматизирует работу с @warpigs_bot"""
+    
 
     strings = {
         "name": "WarPigs",
-        "pig_growth_on": "<emoji document_id=5316581501360420451>🐷</emoji> <b>Авто-рост свиней: <i>Включен!</i></b>",
-        "pig_growth_off": "<emoji document_id=5316581501360420451>🐷</emoji> <b>Авто-рост свиней: <i>Выключен.</i></b>",
-        "pig_fights_on": "<emoji document_id=5316581501360420451>⚔️</emoji> <b>Авто-бои свиней: <i>Включены!</i></b>",
-        "pig_fights_off": "<emoji document_id=5316581501360420451>⚔️</emoji> <b>Авто-бои свиней: <i>Выключены.</i></b>",
+        "pig_growth_on": "<emoji document_id=5316581501360420451>🐷</emoji> <b>Авто-рост свиньи: <i>Включен!</i></b>",
+        "pig_growth_off": "<emoji document_id=5316581501360420451>🐷</emoji> <b>Авто-рост свиньи: <i>Отключен.</i></b>",
+        "pig_fights_on": "<emoji document_id=5316581501360420451>⚔️</emoji> <b>Авто-бой свиньи: <i>Включен!</i></b>",
+        "pig_fights_off": "<emoji document_id=5316581501360420451>⚔️</emoji> <b>Авто-бой свиньи: <i>Отключен.</i></b>",
     }
 
     async def auto_action(self, message, key, command):
@@ -27,10 +28,10 @@ class WarpigsMod(loader.Module):
             await message.respond(f"/{command}")
             await asyncio.sleep(86410)
 
-    @loader.command(ru_doc="Включает/выключает авто-рост свиней.")
     async def agrow(self, message):
+        """Включает/отключает авто-рост"""
         await self.auto_action(message, "pig_growth", "grow")
 
-    @loader.command(ru_doc="Включает/выключает авто-бои свиней.")
     async def afight(self, message):
+        """Включает/отключает авто-бой"""
         await self.auto_action(message, "pig_fights", "fight")
